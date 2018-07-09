e5631023a38e84ad791ff6a8da682cf8274dd58b
Drewbie18 <drew.payette@gmail.com>
2018-07-08 22:11:49 -0400

    created basic simple player program.

---
+++ image-extractor/src/main/java/com/hoser/simple/player/SimplePlayer.java
@@ -0,0 +1,23 @@
+package com.hoser.simple.player;
+
+import uk.co.caprica.vlcj.component.EmbeddedMediaPlayerComponent;
+
+import javax.swing.*;
+
+public class SimplePlayer {
+
+
+    private final JFrame frame;
+
+    private final EmbeddedMediaPlayerComponent mediaPlayerComponent;
+
+    public SimplePlayer(String filePath) {
+        frame = new JFrame("My First Media Player");
+        frame.setBounds(100, 100, 600, 400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mediaPlayerComponent = new EmbeddedMediaPlayerComponent();
+        frame.setContentPane(mediaPlayerComponent);
+        frame.setVisible(true);
+        mediaPlayerComponent.getMediaPlayer().playMedia(filePath);
+    }
+}
